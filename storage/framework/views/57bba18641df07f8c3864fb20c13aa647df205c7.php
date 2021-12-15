cbe22f01428b76e7206e09c7873d5b9815efd487
Shahmir Minhas <62132242+shahmir-minhas@users.noreply.github.com>
2021-12-15 10:17:07 +0500

    main

---
+++ storage/framework/views/57bba18641df07f8c3864fb20c13aa647df205c7.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <link href="<?php echo e(mix('/css/app.css')); ?>" rel="stylesheet" />
+    <script src="<?php echo e(mix('/js/app.js')); ?>" defer></script>
+</head>
+
+<body>
+    <div id="app" data-page="<?php echo e(json_encode($page)); ?>"></div>
+</body>
+
+</html>
+<?php /**PATH F:\jmm.intern\task\Day 38\inertia-react-laravel-template-main\resources\views/app.blade.php ENDPATH**/ ?>
